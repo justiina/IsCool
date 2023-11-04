c308165ca2c3062c2fc9d6bed83a001c8388af7a
justiina <112779447+justiina@users.noreply.github.com>
2023-11-04 15:21:34 +0200

    Images changing with fastening interval

--- app/src/main/java/com/example/iscool/MainActivity.kt
+++ app/src/main/java/com/example/iscool/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.iscool
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -9,19 +11,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.iscool.ui.theme.IsCoolTheme
 import com.example.iscool.ui.theme.Screens.StartGameScreen
 
@@ -66,9 +73,31 @@ fun PictureWithButton(modifier: Modifier=Modifier) {
         12 -> R.drawable.notcool_06
         else -> R.drawable.notcool_07
     }
+
+    val handler = Handler(Looper.getMainLooper())
+
+    DisposableEffect(Unit) {
+        val runnable = object : Runnable {
+            var delayMillis = 3000L // Initial interval is 3 seconds
+            override fun run() {
+                result = (1..13).random()
+                // Reduce the interval gradually (e.g., by 200 milliseconds)
+                delayMillis -= 100
+                delayMillis = delayMillis.coerceAtLeast(400) // Limit to a minimum interval of 400 milliseconds
+
+                handler.postDelayed(this, delayMillis)
+            }
+        }
+        handler.postDelayed(runnable, 3000) // Start with an initial interval of 3 seconds
+
+        onDispose {
+            handler.removeCallbacksAndMessages(null)
+        }
+    }
+
     Column (
         modifier = modifier.fillMaxHeight(),
-        verticalArrangement = Arrangement.SpaceBetween,
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
@@ -79,12 +108,13 @@ fun PictureWithButton(modifier: Modifier=Modifier) {
                 .weight(1f)
         )
         Button(
-            onClick = {result = (1..13).random()},
+            onClick = {/*TODO*/},
+            modifier = Modifier.clip(CircleShape)
+                .size(150.dp)
         ) {
             Text(stringResource(R.string.cool))
         }
     }
 }
-
 
 
