eea96a2ab0e24d6b220718e2ab77a2cb103beace
Cerveku <art.karimaki@gmail.com>
2023-11-10 22:48:41 +0200

    Update StartGameScreen.kt
    Pelin aloitus keskelle ruutua nurkasta

--- app/src/main/java/com/example/iscool/ui/theme/Screens/StartGameScreen.kt
+++ app/src/main/java/com/example/iscool/ui/theme/Screens/StartGameScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.iscool.R
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
 
 @Composable
 fun StartGameScreen(
@@ -17,17 +19,23 @@ fun StartGameScreen(
     modifier: Modifier = Modifier
 ){
     Column(
+        modifier = modifier
+            .fillMaxSize(), // Täyttää koko ruudun
+        verticalArrangement = Arrangement.Top, // Asettaa sisällön yläosaan
+        horizontalAlignment = Alignment.CenterHorizontally // Asettaa sisällön keskelle vaakasuunnassa
     ) {
         Text(
             text = "StartGameScreen",
+            modifier = Modifier.padding(top = 20.dp) // Lisää yläreunaan paddingia
         )
+        Spacer(modifier = Modifier.weight(2f)) // Käyttää 2/3 tilasta ennen nappia
         Button(
-            onClick = {navController.navigate("PlayGameRoute")},
+            onClick = { navController.navigate("PlayGameRoute") },
             modifier = Modifier
-                .size(100.dp)
+                .size(100.dp) // Asettaa napin koon
         ) {
             Text(stringResource(R.string.playButtonText))
         }
+        Spacer(modifier = Modifier.weight(1f)) // Käyttää 1/3 tilasta napin jälkeen
     }
-
 }
