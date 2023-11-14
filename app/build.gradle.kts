db3d852c3c97533d3872d24781a15d94d5886056
Essi Nissinen <essieveliina.nissinen@gmail.com>
2023-11-14 19:23:43 +0200

    more images added to playGameScreen, quit button deleted

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation ("androidx.navigation:navigation-compose:2.7.5")//jos tulee ongelmia kokeile poistaa tämä
-    implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")//jos tulee ongelmia kokeile poistaa tämä
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
+    implementation("com.google.firebase:firebase-inappmessaging-ktx:20.4.0")//jos tulee ongelmia kokeile poistaa tämä
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
